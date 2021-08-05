c6a80f1937af19c42c349b590aca29fb8d5d6281
Rayhan <syahrayhanid@gmail.com>
2021-08-05 20:48:43 +0700

    add username for getting from notes owned

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line camelcase
-const mapDbToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+const mapDbToModel = ({ id, title, body, tags, created_at, updated_at, username }) => ({
   id,
   title,
   body,
   tags,
   createdAt: created_at,
   updatedAt: updated_at,
+  username,
 })
 
 module.exports = { mapDbToModel }
