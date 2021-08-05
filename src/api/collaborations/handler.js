4e540addcf81bcd704b15bfb2953c929913144d3
Rayhan <syahrayhanid@gmail.com>
2021-08-05 15:19:59 +0700

    add delete collaboration handler

--- src/api/collaborations/handler.js
+++ src/api/collaborations/handler.js
@@ -1,3 +1,5 @@
+const ClientError = require('../../exceptions/ClientError')
+
 class CollaborationsHandler {
   constructor (collaborationsService, notesService, validator) {
     this._collaborationsService = collaborationsService
@@ -24,6 +26,40 @@ class CollaborationsHandler {
       })
       response.code(201)
       return response
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        })
+        response.code(error.statusCode)
+        return response
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      })
+      response.code(500)
+      console.error(error)
+      return response
+    }
+  }
+
+  async deleteCollaborationHandler (request, h) {
+    try {
+      this._validator.validateCollaborationPayload(request.payload)
+      const { id: credentialId } = request.auth.credentials
+      const { noteId, userId } = request.payload
+
+      await this._notesService.verifyNoteOwner(noteId, credentialId)
+      await this._collaborationsService.deleteCollaboration(noteId, userId)
+
+      return {
+        status: 'success',
+        message: 'Kolaborasi berhasil dihapus',
+      }
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
